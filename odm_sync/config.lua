f97dd1e6eae27a3df1b4b8dcb7954de593831f4f
Malik™ <73227508+MalikTM@users.noreply.github.com>
2023-02-19 14:25:04 +0100

    Add files via upload

---
+++ odm_sync/config.lua
@@ -0,0 +1,49 @@
+Config = {}
+
+-- Time Settings
+Config.TimeSync = true -- Set it to false if you don't want your time to be synced
+Config.Hour = 12 -- ( This number represents the game time in hours )
+Config.Minute = 0 -- ( Same but with minutes )
+Config.TimerToSyncTime = 30 -- This number represents the timer for every time the script has to sync the time.
+
+-- Weather Settings
+Config.WeatherSync = true -- Set it to false if you don't want your weather to be synced
+Config.WeatherType = "Clear" -- You can choose one from the list below.
+Config.TimerToSyncWeather = 601 -- This number represents the timer for every time the script has to sync the weather.
+
+--[[ 
+    List of Weather :
+    Announce
+    Astronomy
+    Clear
+    Default_PHY
+    FIG_07_Storm
+    ForbiddenForest_01
+    HighAltitudeOnly
+    Intro_01
+    LightClouds_01
+    LightRain_01
+    Misty_01
+    MistyOvercast_01_
+    MKT_Nov11
+    Overcast_01
+    Overcast_Heavy_01
+    Overcast_Heavy_Winter_01
+    Overcast_Windy_01
+    Rainy
+    Sanctuary_Bog
+    Sanctuary_Coastal
+    Sanctuary_Forest
+    Sanctuary_Grasslands
+    Snow_01
+    Snow_Const
+    SnowLight_01
+    SnowShort
+    Stormy_01
+    StormyLarge_01
+    TestStormShort
+    WEA_TestWind
+    Winter_Misty_01
+    Winter_Overcast_01
+    Winter_Overcast_Windy_01
+]]
---
+++ odm_sync/init.lua
@@ -0,0 +1,75 @@
+require("config")
+local players = {}
+local timeBool = false
+local weatherBool = false
+
+function createPlayer(Player)
+    local self = {}
+    self.id = Player.id
+    self.gear = Player.gear
+    self.gender = Player.gender
+    self.house = Player.house
+
+    return self
+end
+
+
+registerForEvent("init", function()
+    if Config.TimeSync then syncTime() timeBool = true end
+    if Config.WeatherSync then syncWeather() weatherBool = true end
+end)
+
+local lasttick = 0
+local timeSync = 0
+local weatherSync = 0
+
+registerForEvent("update", function(delta) -- Syncing Time/Weather
+
+    timeSync = timeSync + delta
+    weatherSync = weatherSync + delta
+
+    if timeSync > Config.TimerToSyncTime and timeBool then
+        timeSync = 0
+        syncTime()
+    end
+    
+    if weatherSync > Config.TimerToSyncWeather and weatherBool then
+        weatherSync = 0
+        syncWeather() -- Do not comment this if you want to sync the weather in your server.
+    end
+end)
+
+
+registerForEvent("player_joined", function(Player) -- Notify when players join
+    players[Player.id] = createPlayer(Player)
+    print("Giocatore ID: "..Player.id.." connesso con la Casata: "..Player.house.." ed il Sesso: ".. Player.gender)
+end)
+
+registerForEvent("player_left", function(Player) -- Notify when players left
+    print("Giocatore ID: "..Player.id.." disconnesso")
+    players[Player.id] = nil
+end)
+
+local hour = Config.Hour
+local minute = Config.Minute
+
+function syncTime()
+    --print("Time is now synced")
+    minute = minute + 2
+
+    if minute > 59 then
+        minute = 0
+        hour = hour + 1
+    end
+
+    world.hour = hour
+    world.minute = minute
+    world:RpcSet()
+end
+
+function syncWeather()
+    --print("Weather is now synced")
+    world.weather = Config.WeatherType -- Choose your favourite weather
+    world:RpcSet()
+end
+
